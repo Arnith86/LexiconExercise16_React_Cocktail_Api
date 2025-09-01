da00a977ed109823514bbd83000f517c8ac0ec35
a21jeaha <hisui86@gmail.com>
2025-09-01 15:19:20 +0200

    api test code implemented.

--- LexiconExercise16_React_Cocktail_Api/src/App.tsx
+++ LexiconExercise16_React_Cocktail_Api/src/App.tsx
@@ -1,4 +1,18 @@
+import { mapRawCocktailData } from "./mapRawCocktailData";
+
+const fetchDrinks = async (): Promise<object> => {
+  const result = await fetch(
+    "https://www.thecocktaildb.com/api/json/v1/1/random.php"
+  );
+
+  const data = await result.json();
+  const someDrink = mapRawCocktailData(data.drinks[0]);
+  console.log(someDrink);
+  return data;
+};
+
 function App() {
+  fetchDrinks();
   return <></>;
 }
 
